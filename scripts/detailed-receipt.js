ed8bbfad2d5d088e8c536f403500ba9e76288ede
Tendai Prince Dzonga <ptdzonga@gmail.com>
2018-04-12 17:47:30 -0400

    now changing receipt status

--- scripts/detailed-receipt.js
+++ scripts/detailed-receipt.js
@@ -94,26 +94,114 @@ function renderReceipt(receipt, details) {
           </tr>
         
         </tr>
+  
+        <tr>
+          <tr>
+            <td> Status  </td>
+            <td style="${renderStatus(details.status)}"> Pending </td> 
+          </tr>
+  
+        </tr>
         
       </table>
   
     </article>
   `
+  //
 }
 
+function viewStatusChange() {
+  return `
+     
+    <article>
+  
+        <p> Change Status </p>
+  
+        <label> new status </label> 
+  
+        <select onchange='changeStatus(event)'>
+    
+          <option value='Ready'> Ready </option>
+  
+          <option value='Picked_Up'> Picked Up </option>
+  
+          <option value='Pending' selected> Pending </option>
+  
+        </select>
+  
+    </article>
+  
+  `
+}
 
 //Actions 
 
-export function populateReceipt() {
+export function changeStatus(e) {
   
-  let receipt = JSON.parse(sessionStorage.getItem('receipt'))
+  let new_status = e.target.value
   let details = JSON.parse(receipt.details)
-  
+  console.log('change', e.target.value)
   console.log(details)
   
-  let article = document.getElementById('detailed-receipt')
+  switch(new_status) {
+    case 'Ready':
+      details.status = { 'ready' : true }
+      populateReceipt(receipt, details)
+      break
+      
+    case 'Picked_Up':
+      details.status = { 'picked_up' : true }
+      populateReceipt(receipt, details)
+      break
+      
+    case 'Pending': 
+      details.status = { 'pending' : true }
+      populateReceipt(receipt, details)
+      break
+  }
   
+}
+
+function renderStatus(status) {
+  
+  let statusMap = new Map(Object.entries(status))
+  
+  if (statusMap.get('pending') === true ) {
+    return `background-color: #E63946`
+  }else if (statusMap.get('ready') === true) {
+    return `background-color: #E0CA3C`
+  }else if (statusMap.get('picked_up') === true ) {
+    return `background-color: #048A81`
+  }
+  
+}
+
+
+export function getReceipt() {
+  
+  receipt = JSON.parse(sessionStorage.getItem('receipt'))
+  let details = JSON.parse(receipt.details)
+  
+  populateReceipt(receipt, details)
+  
+}
+
+export function populateReceipt(receipt, details) {
+  
+  
+  let article = document.getElementById('detailed-receipt')
+
   article.innerHTML = renderReceipt(receipt, details)
+  
+  
+}
+
+export function populateStatusChange() {
+  
+  let article = document.getElementById('new-status')
+  
+  article.innerHTML = viewStatusChange()
+  
 }
 
 
